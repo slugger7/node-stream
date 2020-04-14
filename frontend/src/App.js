1fee5ac9269aac0992fa949f16cff6da4dafb2ef
Kevin Heritage <kheritage222@gmail.com>
2020-04-14 20:44:09 +0200

    Stream a video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const directory = '/mnt/videos/';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video id="videoPlayer" controls muted="muted" autoplay> 
+        <source src={`http://localhost:8001/video/${encodeURIComponent(directory)}sample1.mp4`} type="video/mp4" />
+    </video>
     </div>
   );
 }
